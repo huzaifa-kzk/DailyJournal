f6bac21918a238ac466c3eb91a59d4e15e72635a
rayyanibnnaveed <rayyanibnnaveed@gmail.com>
2026-02-16 20:59:58 +0500

    Fourth Commit

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,16 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
+
 
 const authRoutes = require("./routes/auth");
 const postRoutes = require("./routes/posts");
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "frontend")));
 app.use(cors());
 app.use(express.json());
 
@@ -15,7 +19,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Server Running");
+  res.sendFile(path.join(__dirname, "frontend", "login.html"));
 });
 
 app.listen(3000, () => {
